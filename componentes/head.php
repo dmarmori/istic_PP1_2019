dac26edc23daefddb268499358d8437b7dde0842
dmarmori <dmarmori@iiconcepcion.edu.ar>
2019-11-13 01:34:43 -0300

    prueba15

--- componentes/head.php
+++ componentes/head.php
@@ -3,13 +3,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="./img/IconoEsta.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="../img/IconoEsta.png" sizes="16x16" type="image/png">
 
     <!-- Bootstrap core CSS -->
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="./css/floating-labels.css" rel="stylesheet">
+    <link href="../css/floating-labels.css" rel="stylesheet">
   
     
