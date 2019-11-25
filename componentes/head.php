6a62f6f1e42d7bf96df81e6b3de7ccca9ba5c452
dmarmori <dmarmori@iiconcepcion.edu.ar>
2019-11-25 20:29:25 -0300

    Estilo6

--- componentes/head.php
+++ componentes/head.php
@@ -5,7 +5,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../img/IconoEsta.png" sizes="16x16" type="image/png">
 
-    <link rel="stylesheet" type="text/css" href="/Marmori/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 
     <!-- Bootstrap core CSS -->
     <link href="../../css/bootstrap.min.css" rel="stylesheet">
